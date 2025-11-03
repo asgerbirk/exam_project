a571ff83aa49db7d20192f571dcf2c5fb8245207
asgerbirk <asger.birk@gmail.com>
2025-11-03 22:09:38 +0100

    test

--- q_functions.js
+++ q_functions.js
@@ -1,6 +1,6 @@
 // --------- MQTT FUNKTIONER -------moja2024 ------
 
-const topic = "HUSK_AT_SKIFTE";
+const topic = "UNIK";
 const afsenderID = "P5";
 
 const client = mqtt.connect("wss://broker.hivemq.com:8884/mqtt");
